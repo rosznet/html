818da13292d21211d1e80ec92efde13805a0bddd
rosznet <76278769+rosznet@users.noreply.github.com>
2026-01-22 09:03:30 +0100

    Add files via upload

---
+++ tabliczka30/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/tabliczka.html',
+        '/Digital-7.ttf'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request).catch(() => {
+          return caches.match('/tabliczka.html');
+        });
+      })
+  );
+});
